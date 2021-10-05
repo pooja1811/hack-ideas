070eba40d84f0759be9089c5f327e7850f500ea8
Pooja Sheth <pooja18sheth@gmail.com>
2021-10-05 20:55:21 +0530

    Added vuetify and mini-toastr

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,11 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import vuetify from "./plugins/vuetify";
+import toast from "./plugins/mini-toastr.js";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  vuetify,
+  toast,
   render: (h) => h(App),
 }).$mount("#app");
